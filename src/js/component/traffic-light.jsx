02ac47c28ec10c9c2bf1dbbafed841afef576a86
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-03-27 07:01:34 +0000

    final traffic light design and color options

--- src/js/component/traffic-light.jsx
+++ src/js/component/traffic-light.jsx
@@ -2,12 +2,16 @@ import React from "react";
 
 const TrafficLight = () => {
 	return(
-		<div className="traffic-light d-flex flex-column gap-4">
-            <div className="light turned-off"></div>
-			<div className="light turned-off"></div>
-			<div className="light turned-on"></div>
+		<div className=" fixed-bottom d-flex flex-column justify-content-center align-items-center">
+			<div className="traffic-light d-flex flex-column gap-4">
+				<div className="light turned-on red"></div>
+				<div className="light turned-off yellow"></div>
+				<div className="light turned-off green"></div>
+			</div>
+			<div className="bar"></div>
 		</div>
 	)
 }
 
 export default TrafficLight;
+
