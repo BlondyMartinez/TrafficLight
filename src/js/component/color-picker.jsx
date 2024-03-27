84ad979d17fb5b878b34a3e2bd3e32b007928537
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-03-27 14:00:29 +0000

    separated colorpicker from traffic light controller

---
+++ src/js/component/color-picker.jsx
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from "react";
+
+const ColorPicker = (props) => {
+    const [color, setColor] = useState({
+		red: false,
+		yellow: false,
+		green: false,
+		blue: false,
+		purple: false,
+		pink: false
+	})
+
+	const handleColorClick = (selectedColor) => {
+        setColor(prevColors => {
+            const updatedColors = {};
+            for (const color in prevColors) {
+                updatedColors[color] = color === selectedColor;
+            }
+            return updatedColors;
+        });
+        props.onSelectColor(selectedColor, props.identifier);
+    };
+
+    useEffect(() => {
+        setColor(prevColors => ({
+            ...prevColors,
+            [props.initialSelected]: true
+        }));
+    }, [props.initialSelected]);
+
+    return(
+        <div className="color-section p-2 m-4">
+            <div>
+                <span 
+                    className={`red color-option ${color.red ? 'selected' : ''}`}
+                    onClick={() => handleColorClick('red')}
+                >⬤ </span>
+                <span 
+                    className={`yellow color-option ${color.yellow ? 'selected' : ''}`}
+                    onClick={() => handleColorClick('yellow')}
+                >⬤ </span>
+                <span 
+                    className={`green color-option ${color.green ? 'selected' : ''}`}
+                    onClick={() => handleColorClick('green')}
+                >⬤ </span>
+                <span 
+                    className={`blue color-option ${color.blue ? 'selected' : ''}`}
+                    onClick={() => handleColorClick('blue')}
+                >⬤ </span>
+                <span 
+                    className={`purple color-option ${color.purple ? 'selected' : ''}`}
+                    onClick={() => handleColorClick('purple')}
+                >⬤ </span>
+                <span 
+                    className={`pink color-option ${color.pink ? 'selected' : ''}`}
+                    onClick={() => handleColorClick('pink')}
+                >⬤</span>
+            </div>
+        </div>
+	)
+}
+
+export default ColorPicker;
