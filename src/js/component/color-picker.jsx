567c3c52f10028fe82a0fefc983be1761c4a3c19
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-03-27 21:12:37 +0000

    now rainbow mode works properly

--- src/js/component/color-picker.jsx
+++ src/js/component/color-picker.jsx
@@ -10,29 +10,14 @@ const ColorPicker = (props) => {
 		purple: false,
 		pink: false,
         rainbow: false
-	})
+	});
+
+    const [rainbowIntervalId, setRainbowIntervalId] = useState(null);
+    const [rainbowMode, setRainbowMode] = useState(false);
 
 	const handleColorClick = (selectedColor) => {
-        if (selectedColor === 'rainbow') {
-            if (!color.rainbow) {
-                const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink'];
-                let index = 0;
-                const intervalId = setInterval(() => {
-                    props.onSelectColor(colors[index], props.identifier);
-                    index = (index + 1) % colors.length;
-                }, 500); 
-                setRainbowIntervalId(intervalId);
-            }
-        } else {
-            setColor(prevColors => {
-                const updatedColors = {};
-                for (const color in prevColors) {
-                    updatedColors[color] = color === selectedColor;
-                }
-                return updatedColors;
-            });
-            props.onSelectColor(selectedColor, props.identifier);
-        }
+        if (selectedColor === 'rainbow') rainbowWaveMode();
+        else colorMode();
     };
 
     useEffect(() => {
@@ -42,40 +27,64 @@ const ColorPicker = (props) => {
         }));
     }, [props.initialSelected]);
 
+    function rainbowWaveMode() {
+        setRainbowMode(true);
+        const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink'];
+        let index = 0;
+        const intervalId = setInterval(() => {
+            props.onSelectColor(colors[index], props.identifier);
+            index = (index + 1) % colors.length;
+        }, 1000); 
+        setRainbowIntervalId(intervalId);
+    }
+
+    function colorMode(){
+        setRainbowMode(false);
+        setColor(prevColors => {
+            const updatedColors = {};
+            for (const color in prevColors) {
+                updatedColors[color] = color === selectedColor;
+            }
+            return updatedColors;
+        });
+        clearInterval(rainbowIntervalId);
+        props.onSelectColor(selectedColor, props.identifier);
+    }
+
     return(
         <div className="color-section p-2 m-4">
             <div>
                 <span 
-                    className={`red color-option ${color.red ? 'selected' : ''}`}
+                    className={`red color-option ${color.red && !rainbowMode ? 'selected' : ''}`}
                     onClick={() => handleColorClick('red')}
                 >⬤ </span>
                 <span 
-                    className={`orange color-option ${color.orange ? 'selected' : ''}`}
+                    className={`orange color-option ${color.orange && !rainbowMode ? 'selected' : ''}`}
                     onClick={() => handleColorClick('orange')}
                 >⬤ </span>
                 <span 
-                    className={`yellow color-option ${color.yellow ? 'selected' : ''}`}
+                    className={`yellow color-option ${color.yellow && !rainbowMode ? 'selected' : ''}`}
                     onClick={() => handleColorClick('yellow')}
                 >⬤ </span>
                 <span 
-                    className={`green color-option ${color.green ? 'selected' : ''}`}
+                    className={`green color-option ${color.green && !rainbowMode ? 'selected' : ''}`}
                     onClick={() => handleColorClick('green')}
                 >⬤ </span>
                 <span 
-                    className={`blue color-option ${color.blue ? 'selected' : ''}`}
+                    className={`blue color-option ${color.blue && !rainbowMode ? 'selected' : ''}`}
                     onClick={() => handleColorClick('blue')}
                 >⬤ </span>
                 <span 
-                    className={`purple color-option ${color.purple ? 'selected' : ''}`}
+                    className={`purple color-option ${color.purple && !rainbowMode ? 'selected' : ''}`}
                     onClick={() => handleColorClick('purple')}
                 >⬤ </span>
                 <span 
-                    className={`pink color-option ${color.pink ? 'selected' : ''}`}
+                    className={`pink color-option ${color.pink && !rainbowMode ? 'selected' : ''}`}
                     onClick={() => handleColorClick('pink')}
                 >⬤ </span>
                 
                 <span 
-                    className={`color-option ${color.rainbow ? 'rainbow' : ''}`}
+                    className={`color-option ${rainbowMode ? 'rainbow' : ''}`}
                     onClick={() => handleColorClick('rainbow')}
                     ><i className="fa-solid fa-rainbow"></i></span>
             </div>
