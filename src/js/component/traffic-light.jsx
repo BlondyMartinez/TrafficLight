2b46c1cbd7f3da9de54894ef22af764eb0897322
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-03-27 07:37:04 +0000

    now lights turn on and off on click

--- src/js/component/traffic-light.jsx
+++ src/js/component/traffic-light.jsx
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 const TrafficLight = () => {
+	const [lights, setLights] = useState({
+		top: true,
+		mid: false,
+		bottom: false
+	})
+
+	const handleLightClick = (position) => {
+		const updatedLights = {};
+
+		Object.keys(lights).forEach((key) => {
+		  updatedLights[key] = key === position;
+		});
+
+		setLights(updatedLights);
+	};
+	
+
 	return(
 		<div className=" fixed-bottom d-flex flex-column justify-content-center align-items-center">
 			<div className="traffic-light d-flex flex-column gap-4">
-				<div className="light turned-on red"></div>
-				<div className="light turned-off yellow"></div>
-				<div className="light turned-off green"></div>
+				<div
+					className={`light ${lights.top ? 'turned-on' : ''} red`}
+					onClick={() => handleLightClick('top')}
+				></div>
+				<div
+					className={`light ${lights.mid ? 'turned-on' : ''} yellow`}
+					onClick={() => handleLightClick('mid')}
+				></div>
+				<div
+					className={`light ${lights.bottom ? 'turned-on' : 'turned-off'} green`}
+					onClick={() => handleLightClick('bottom')}
+				></div>
 			</div>
 			<div className="bar"></div>
 		</div>
