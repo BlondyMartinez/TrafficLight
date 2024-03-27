9f5bf7a71d3fa6881de90c7240f07ad20037381c
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-03-27 14:49:54 +0000

    styled controller as billboard

--- src/js/component/traffic-light-controller.jsx
+++ src/js/component/traffic-light-controller.jsx
@@ -3,10 +3,16 @@ import ColorPicker from "./color-picker";
 
 const TrafficLightController = ({ selectedColors, onSelectColor }) => {
     return(
-        <div className="controller ms-5">
-            <ColorPicker initialSelected={selectedColors.top} identifier='top' onSelectColor={onSelectColor} />
-            <ColorPicker initialSelected={selectedColors.mid} identifier='mid' onSelectColor={onSelectColor} />
-            <ColorPicker initialSelected={selectedColors.bottom} identifier='bottom' onSelectColor={onSelectColor} />
+        <div className="d-flex flex-column position-absolute bottom-0 start-60">
+            <div className="controller">
+                <ColorPicker initialSelected={selectedColors.top} identifier='top' onSelectColor={onSelectColor} />
+                <ColorPicker initialSelected={selectedColors.mid} identifier='mid' onSelectColor={onSelectColor} />
+                <ColorPicker initialSelected={selectedColors.bottom} identifier='bottom' onSelectColor={onSelectColor} />
+            </div>
+            <div className="d-flex justify-content-around">
+                <div className="controller-bar"></div>
+                <div className="controller-bar"></div>
+            </div>
         </div>
     );
 }
