f67595163bc6eec580dc9e557bd9f1f5b3b6d574
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-03-28 14:26:12 +0000

    blink animation

--- src/js/component/billboard.jsx
+++ src/js/component/billboard.jsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Logo from "../../img/bm.png";
 
 const Billboard = () => {
+    const [turnOff, setTurnOff] = useState(false);
+    
+    useEffect(() => {
+        const intervalId = setInterval(blink, 2000);
+
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, []); 
+
+    function blink(){
+        if (Math.random() > 0.3) return
+        let delay = 0;
+        for(let i = 0; i < 4; i++) {
+            setTimeout(() => {
+                setTurnOff(prevTurnOff => !prevTurnOff);
+            }, delay);
+            delay += i === 3 ? 300 : 100 * (i + 1); 
+        }
+    }
+
+    console.log(turnOff)
+
     return(
-        <div className="billboard">
-            <a href="https://www.linkedin.com/in/blondy-martinez/" target="_blank"><img className="billboard-img" src={Logo} alt="" /></a>
+        <div className={`billboard ${turnOff ? 'billboard-off' : ''}`}>
+            <a href="https://www.linkedin.com/in/blondy-martinez/" target="_blank"><img className={`billboard-img  ${turnOff ? 'billboard-off-img' : ''}`} src={Logo} alt="" /></a>
         </div>
     );
 }
