098b32a811090d5b93bbb115ebe5415f5146d222
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-03-28 12:32:40 +0000

    now it takes props instead of individual properties

--- src/js/component/traffic-light-controller.jsx
+++ src/js/component/traffic-light-controller.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import ColorPicker from "./color-picker";
+import LightCycle from "./light-cycle";
 
-const TrafficLightController = ({ selectedColors, onSelectColor }) => {
+const TrafficLightController = (props) => {
     return(
         <div className="d-flex flex-column position-absolute bottom-0 start-60">
             <div className="controller">
-                <ColorPicker initialSelected={selectedColors.top} identifier='top' onSelectColor={onSelectColor} />
-                <ColorPicker initialSelected={selectedColors.mid} identifier='mid' onSelectColor={onSelectColor} />
-                <ColorPicker initialSelected={selectedColors.bottom} identifier='bottom' onSelectColor={onSelectColor} />
+                <LightCycle onSelectPosition={props.onSelectPosition} />
+                <ColorPicker initialSelected={props.selectedColors.top} identifier='top' onSelectColor={props.onSelectColor} />
+                <ColorPicker initialSelected={props.selectedColors.mid} identifier='mid' onSelectColor={props.onSelectColor} />
+                <ColorPicker initialSelected={props.selectedColors.bottom} identifier='bottom' onSelectColor={props.onSelectColor} />
             </div>
             <div className="d-flex justify-content-around">
                 <div className="controller-bar"></div>
